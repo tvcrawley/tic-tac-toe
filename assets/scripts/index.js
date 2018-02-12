6041f563a20a12491a314f8b70024fd12725d1a4
tvlangley <chibia2@gmail.com>
2018-02-12 10:28:20 -0500

    Able to add to gameboard
    Add to gameboard according to index with specified value

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,31 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+$(() => {
+  // empty gameboard
+  const gameboard = ['', '', '', '', '', '', '', '', '']
+  const entry = 'X'
+  const entryPoint = 3
+  // ADD TO BOARD
+  // loop through the board
+  // if index, change value
+  gameboard.forEach((piece, index) => {
+    // if index equals place on visual board
+    if (index === entryPoint) {
+      if (entry === 'X') {
+        return gameboard.splice(index, 1, 'X')
+      } else if (entry === 'O') {
+        return gameboard.splice(index, 1, 'O')
+      }
+    }
+    return gameboard
+  })
+  console.log('gameboard: ', gameboard)
+
+// next move/switch player
+
+// check winner
+})
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
