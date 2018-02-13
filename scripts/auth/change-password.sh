ca1ddc32cbb2febc6ed6f92bdebfac031c1326d7
tvlangley <chibia2@gmail.com>
2018-02-13 09:43:31 -0500

    Add auth scripts
    Add authentication curl scripts for API testing purposes

---
+++ scripts/auth/change-password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://tic-tac-toe.wdibos.com/change-password/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLD_PASSWORD}"'",
+      "new": "'"${NEW_PASSWORD}"'"
+    }
+  }'
+
+echo
---
+++ scripts/auth/sign-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://tic-tac-toe.wdibos.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+    }
+  }'
+
+echo
