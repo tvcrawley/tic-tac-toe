e58f1d18c9a6b571411cfb380999725d62d4eb23
tvlangley <chibia2@gmail.com>
2018-02-14 14:25:26 -0500

    Add game scripts
    Add game curl scripts for API testing purposes

---
+++ scripts/game/create.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl "http://tic-tac-toe.wdibos.com/games" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+
+echo
